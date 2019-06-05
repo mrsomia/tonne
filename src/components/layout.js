b3971ced4c0000c5e6674b08ec2abbef829f8cab
Sach <sach@Sachs-MacBook-Pro.local>
2019-06-05 12:00:53 +0100

    Adds styles to index and layout keeping footer at bottom

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,12 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header2"
+import Header from "./header3"
 import Footer from "./footer"
-// import tachyons from 'tachyons'
+//eslint-disable-next-line
+import tachyons from 'tachyons'
 // import "./layout.css"
-import "./tachyons.css"
+// import "./tachyons.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -27,9 +28,11 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <div className="sans-serif bg-black white">
-        <Header siteTitle={data.site.siteMetadata.title} />
+      <div className="sans-serif bg-black white flex flex-column min-vh-100">
+        <div style={{flex: `1`}}>
+          <Header siteTitle={data.site.siteMetadata.title} />
           {children}
+        </div>
         <Footer />
       </div>
     )}
