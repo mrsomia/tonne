b94723eb8c8f2ebbd459d9e494ae2063c5855b9e
Sach <sach@Sachs-MacBook-Pro.local>
2019-06-12 17:00:24 +0100

    refactors css for not small displays

--- src/components/header3.js
+++ src/components/header3.js
@@ -27,14 +27,14 @@ const Header = ({siteTitle}) => {
   const toggleNav = () => {document.getElementById("navigation").classList.toggle("nav-open")}
   return(
     <div className="nav flex flex-column flex-row-ns" id="navigation">
-        <div className="title pa2 tc">
+        <div className="title pa2 ph3 tc menu-push-left">
           <Link to="/" className="no-underline white" onClick={toggleNav}>
             <h1 className="f2">{siteTitle}</h1>
           </Link>
         </div>
         <div className="tc">
           <Link to="/menu" className="no-underline yellow" onClick={toggleNav}>
-            <h1 className="f2">Menu</h1>
+            <h1 className="f2 pa2 ph3">Menu</h1>
           </Link>
         </div>
         <span className="white pointer absolute top-0 right-0 pa3 f1 dn-ns"
