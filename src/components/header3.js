8f4d7256f8c9b3d8e5bb289446023f4ad51a2ad3
Sach <sach@Sachs-MacBook-Pro.local>
2019-06-12 16:47:44 +0100

    adds link functionality to header

--- src/components/header3.js
+++ src/components/header3.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { Link } from 'gatsby'
+import { Link } from 'gatsby'
 import "./header3.css"
 
 /* const Header = ({siteTitle}) => {
@@ -28,14 +28,18 @@ const Header = ({siteTitle}) => {
   return(
     <div className="nav flex flex-column flex-row-ns" id="navigation">
         <div className="title pa2 tc">
-          <h1 className="f2">{siteTitle}</h1>
-          <span className="white pointer absolute top-0 right-0 pa3 f1"
-          onClick={toggleNav}
-          >&times;</span>
+          <Link to="/" className="no-underline white" onClick={toggleNav}>
+            <h1 className="f2">{siteTitle}</h1>
+          </Link>
         </div>
         <div className="tc">
-          <h1 className="f3">Menu</h1>
+          <Link to="/menu" className="no-underline yellow" onClick={toggleNav}>
+            <h1 className="f2">Menu</h1>
+          </Link>
         </div>
+        <span className="white pointer absolute top-0 right-0 pa3 f1 dn-ns"
+        onClick={toggleNav}
+        >&times;</span>
     </div>
   )
 }
